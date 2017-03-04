d425c4cef4a16121767561dae0cdf07ea1b79f9f
alicja-cyganiewicz <alicja.cyganiewicz@infakt.pl>
2017-03-04 23:53:01 +0000

    Confirming, rejecting or next notification

--- src/index.js
+++ src/index.js
@@ -126,38 +126,62 @@ var startGetUsersHandlers = Alexa.CreateStateHandler(states.CHOOSE_ABILITY, {
 });
 
 var notificationsHandlers = Alexa.CreateStateHandler(states.CHECK_NOTIFICATION, {
-    // 'getUsersWithAbility': function () {
-    //   this.attributes['ability_name'] = this.event.request.intent.slots.ability.value;
-    //   var ability = this.attributes['ability_name'];
-    //   var that = this;
-    //   getJSON(this).then(function (response) {
-    //     if ( response.length > 0 ) {
-    //       that.attributes['chosen_person_data'] = response[0].user;
-    //       var output = "I've found "+ that.attributes['chosen_person_data'].first_name +", do you like my choice?";
-    //       that.emit(':ask', output);
-    //     } else {
-    //       that.emit(':tell', "I did't find any experts in "+ability+". Try to define the skill in a different way.");
-    //     }
-    //   });
-    // },
-    // 'getNextUser': function () {
-    //   var ability = this.attributes['ability_name'];
-    //   var that = this;
-    //   getJSON(this).then(function (response) {
-    //     if ( response.length > 0 ) {
-    //       that.attributes['chosen_person_data'] = response[0].user;
-    //       var output = "I've found "+ that.attributes['chosen_person_data'].first_name +", do you like my choice?";
-    //       that.emit(':ask', output);
-    //     } else {
-    //       that.emit(':tell', "I did't find any other experts in "+ability+". Try to define the skill in a different way.");
-    //     }
-    //   });
-    // },
+    'getNextUser': function () {
+      var current_notification = this.attributes['unconfirmed_notifications'][this.attributes['current_notification']];
+      if (current_notification) {
+        this.attributes['current_notification'] = this.attributes['current_notification'] + 1;
+        var output = "Invitation from "+ current_notification.meeting.user.first_name + ". Confirm, reject, next invitation, or pass to looking for a lunch?";
+        this.emit(':ask', output);
+      } else {
+        this.handler.state = states.CHOOSE_ABILITY;
+        var output = "No more waiting invitations. You're looking for a person with what skill?";
+        this.emit(':ask', output);
+      }
+    },
     'AMAZON.YesIntent': function () {
         var current_notification = this.attributes['unconfirmed_notifications'][this.attributes['current_notification']];
         this.attributes['current_notification'] = this.attributes['current_notification'] + 1;
         var output = "Invitation from "+ current_notification.meeting.user.first_name + ". Confirm, reject, next invitation, or pass to looking for a lunch?";
         this.emit(':ask', output);
+    },
+    'passToSearch': function () {
+        this.handler.state = states.CHOOSE_ABILITY;
+        output = welcomeMessage;
+        this.emit(':ask', output, welcomeRepromt);
+    },
+    'confirmInvitation': function () {
+        var that = this;
+        postConfirmInvitaionJSON(this).then(function (response) {
+          var output = "Meeting successfully confirmed.";
+          that.attributes['current_notification'] = that.attributes['current_notification'] + 1;
+          if (that.attributes['unconfirmed_notifications'][that.attributes['current_notification']]) {
+            var current_notification = that.attributes['unconfirmed_notifications'][that.attributes['current_notification']];
+            that.attributes['current_notification'] = that.attributes['current_notification'] + 1;
+            output = output + "Invitation from "+ current_notification.meeting.user.first_name + ". Confirm, reject, next invitation, or pass to looking for a lunch?";
+            that.emit(':ask', output);
+          } else {
+            that.handler.state = states.CHOOSE_ABILITY;
+            output = output + "No more waiting invitations. You're looking for a person with what skill?";
+            that.emit(':ask', output);
+          }
+        });
+    },
+    'rejectInvitation': function () {
+        var that = this;
+        postRejectInvitaionJSON(this).then(function (response) {
+          var output = "Meeting successfully rejected.";
+          that.attributes['current_notification'] = that.attributes['current_notification'] + 1;
+          if (that.attributes['unconfirmed_notifications'][that.attributes['current_notification']]) {
+            var current_notification = that.attributes['unconfirmed_notifications'][that.attributes['current_notification']];
+            that.attributes['current_notification'] = that.attributes['current_notification'] + 1;
+            output = output + "Invitation from "+ current_notification.meeting.user.first_name + ". Confirm, reject, next invitation, or pass to looking for a lunch?";
+            that.emit(':ask', output);
+          } else {
+            that.handler.state = states.CHOOSE_ABILITY;
+            output = output + "No more waiting invitations. You're looking for a person with what skill?";
+            that.emit(':ask', output);
+          }
+        });
     },
     'AMAZON.NoIntent': function () {
         this.handler.state = states.CHOOSE_ABILITY;
@@ -227,6 +251,34 @@ function postJSON(that) {
       body: {
           invitor_id: that.attributes['current_user_id'],
           invited_id: that.attributes['chosen_person_data'].id,
+      },
+      json: true // Automatically stringifies the body to JSON
+    };
+
+    return rq(options);
+}
+
+function postConfirmInvitaionJSON(that) {
+
+    var options = {
+      method: 'POST',
+      uri: "https://hidden-beach-26730.herokuapp.com/api/confirm",
+      body: {
+          meeting_id: that.attributes['current_notification']
+      },
+      json: true // Automatically stringifies the body to JSON
+    };
+
+    return rq(options);
+}
+
+function postRejectInvitaionJSON(that) {
+
+    var options = {
+      method: 'POST',
+      uri: "https://hidden-beach-26730.herokuapp.com/api/reject",
+      body: {
+          meeting_id: that.attributes['current_notification']
       },
       json: true // Automatically stringifies the body to JSON
     };
